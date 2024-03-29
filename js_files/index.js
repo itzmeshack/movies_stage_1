9beb5a2b66791be46b493f77dafb5d0a444c581b
itzmeshack <meshackomon.dev@gmail.com>
2024-03-29 12:14:23 +0000

    added more cotent and some changes been done.

---
+++ js_files/index.js
@@ -0,0 +1,21 @@
+/*
+
+let links = document.querySelectorAll('.anchor-links');
+
+
+links.forEach(function(link) {
+    link.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent default link behavior
+
+        // Remove 'active' class from all links
+        links.forEach(function(link) {
+            link.classList.remove("active");
+        });
+
+        // Add 'active' class to the clicked link
+        link.classList.add("active");
+    });
+});
+
+
+*/
