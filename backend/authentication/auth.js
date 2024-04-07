8bf862e2f18d9dffba8eb67456c2144f34fa2da6
itzmeshack <meshackomon.dev@gmail.com>
2024-04-07 18:09:34 +0100

    new changes made to entire pages

--- backend/authentication/auth.js
+++ backend/authentication/auth.js
@@ -1,11 +1,33 @@
+const exp = require('constants');
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.listen(5000);
+
+const port = 3000;
+
+app.listen(port, ()=>{
+    console.log('server running successfully')
+})
 
 app.set('view-engine', 'ejs');
 
+//static
 
+
+app.use(express.static('public'));
+app.use('/stylesheet', express.static(__dirname + 'public/stylesheet'));
+app.use('/images_files', express.static(__dirname + 'public/images_files'));
+
+//renders
+
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
 app.get('/login', (req, res) => {
     res.render('login.ejs')
+})
+app.get('/register', (req,res) =>{
+    res.render('register.ejs')
 })
