414c384e72476cf1bb749c060f2c7f8184d0c5b9
itzmeshack <meshackomon.dev@gmail.com>
2024-04-07 23:08:52 +0100

    creating routing for each files

--- backend/authentication/auth.js
+++ backend/authentication/auth.js
@@ -2,6 +2,7 @@ const exp = require('constants');
 const express = require('express');
 const app = express();
 const path = require('path');
+const bcrypt = require('bcrypt');
 
 
 const port = 3000;
@@ -27,19 +28,52 @@ app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
-})
+});
 app.get('/login', (req, res) => {
     res.render('login.ejs')
-})
+});
 app.get('/register', (req,res) =>{
     res.render('register.ejs')
-})
+});
+
+app.get('/home', (req, res) =>{
+    res.render('home.ejs')
+});
+
+app.get('/movies', (req, res) => {
+    res.render('movies.ejs')
+});
+
+app.get('/topimdb', (request, response) => {
+    response.render('topimdb.ejs')
+});
+
+app.get('/tvseries', (req, res) => {
+    res.render('tvseries.ejs')
+});
+
+
 
 
 //post 
 
 
-app.get( '/register', (req, res) =>{
+app.post( '/register', async (req, res) =>{
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        user.push({
+            //id: Date.now().toString,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('login');
+
+    }catch{
+        res.redirect('register')
+
+    }
+
+    console.log(user)
 
 });
 
